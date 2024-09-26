86102a935e6012d14af6b1e4ef6e10f19dcc1f81
echonorthernlights <elhassanianas@gmail.com>
2024-09-26 13:09:07 +0100

    files fix

--- bone_api/obj/Debug/net8.0/bone_api.AssemblyInfo.cs
+++ bone_api/obj/Debug/net8.0/bone_api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("bone_api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c224fd2226d56addda6b1988f7bb1a6b8485012")]
 [assembly: System.Reflection.AssemblyProductAttribute("bone_api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("bone_api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
